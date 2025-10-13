afdb37fd75f5e3296d014359fd2f1c6634deb83e
misung.dev <misung.dev@gmail.com>
2025-10-14 00:16:18 +0900

    chore: 스토리북에 vitest 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,22 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [],
-  framework: {
-    name: '@storybook/nextjs',
-    options: {},
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs-vite",
+    "options": {}
   },
-  staticDirs: ['../public'],
+  "staticDirs": [
+    "../public"
+  ]
 };
 export default config;
