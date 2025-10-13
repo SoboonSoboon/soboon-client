c3b94cf6f4d3a32d64a653c8210f95efbc50792b
jihee <sweet9811@gmail.com>
2025-10-13 14:10:14 +0900

    design: 메인페이지 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { Button } from '@/components';
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className="mx-auto w-[1200px]">
+      <div className="direction-row flex items-center justify-between pt-[100px]">
+        <div>
+          <h1 className="weight-400 leading-1.2 text-[54px]">
+            함께 사서, <br />
+            알뜰하게 나누는 소비
+          </h1>
+          <p className="weight-400 my-6">
+            대용량 제품을 같이 사서, 필요한 만큼만 소분해요.
+          </p>
+          <Button primary label="시작하기" className="mt-[45px]" />
+        </div>
+        <Image
+          className="rounded-[60px]"
+          src="/images/soboon-tokki.png"
+          alt="Main Image"
+          width={534}
+          height={509}
+        />
+      </div>
     </div>
   );
 }
