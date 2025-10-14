57762393ad5c9846e4370b6292bdca014b0f8a74
keeprok <140785214+keeprok@users.noreply.github.com>
2025-10-14 17:32:02 +0900

    refactor: 중첩 index삭제

--- src/components/Molecules/modal/index.ts
+++ src/components/Molecules/modal/index.ts
@@ -1,7 +1,14 @@
-export { useModalEscape } from './hooks/useModalEscape';
-export { useModalScrollLock } from './hooks/useModalScrollLock';
+// Modal 컴포넌트
+export { Modal } from './modal';
 
+// Modal 타입들
 export type { ModalProps, ModalPosition, ModalSize } from './utils/Modal.types';
+
+// Modal 유틸리티 함수들
 export { getPositionClass, getSizeClass } from './utils/Modal.utils';
 
-export { Modal } from './modal';
+// Modal 훅들
+export { useModalEscape } from './hooks/useModalEscape';
+export { useModalScrollLock } from './hooks/useModalScrollLock';
+export { useModal } from './hooks/useModal';
+export type { UseModalOptions, UseModalReturn } from './hooks/useModal';
