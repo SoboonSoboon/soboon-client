13e2de92ebfa41d7608abc3509d1a9dde4eff92f
kim dong hyeon <kdh990315@naver.com>
2025-10-16 02:38:28 +0900

    feat: 모임 등록하기 구현

---
+++ src/app/(marketplace)/components/registerModal/ShoppingRegisterForm.tsx
@@ -0,0 +1,197 @@
+'use client';
+
+import { TextInput } from '@/components';
+import { modelProvinceOptions } from '@/constants/locations';
+import { modelCityOptions } from '@/constants/locations';
+import { modelDistrictOptions } from '@/constants/locations';
+import { Textarea } from '@/components';
+import { Button } from '@/components';
+import { useMemo, useState } from 'react';
+import { shoppingRegisterApi } from '@/apis/meetings/registerApi';
+import { useToast } from '@/components/Atoms';
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { ApiResponse } from '@/types/common';
+import { XIcon } from 'lucide-react';
+
+interface ShoppingRegisterFormProps {
+  handleClose: () => void;
+}
+
+export function ShoppingRegisterForm({
+  handleClose,
+}: ShoppingRegisterFormProps) {
+  const { success, error } = useToast();
+  const router = useRouter();
+
+  const [formData, setFormData] = useState({
+    title: '',
+    province: '',
+    city: '',
+    district: '',
+    detail: '',
+    description: '',
+    capacity: 5, // 디자인에 인원 설정이 누락되어 임시로 5명으로 설정
+  });
+
+  const isValid = useMemo(() => {
+    return (
+      formData.title &&
+      formData.province &&
+      formData.city &&
+      formData.district &&
+      formData.detail &&
+      formData.description
+    );
+  }, [formData]);
+
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      const response = await shoppingRegisterApi(formData);
+      return response;
+    },
+    onSuccess: (data: ApiResponse<string>) => {
+      success(data.message!);
+      router.push('/shopping');
+      router.refresh();
+      handleClose();
+    },
+    onError: (data: ApiResponse<string>) => {
+      error(data.message!);
+      handleClose();
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    mutate();
+  };
+
+  return (
+    <>
+      <div className="mb-8 flex items-center justify-between">
+        <h1 className="text-2xl font-bold">장보기 모임 등록</h1>
+        <button onClick={handleClose} className="cursor-pointer">
+          <XIcon className="h-4 w-4" />
+        </button>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-6">
+          <label htmlFor="title" className="mb-3 block text-[#1F2937]">
+            어떤 장보기 모임을 만들까요?
+          </label>
+          <TextInput
+            id="title"
+            required
+            placeholder="ex) 트레이더스 고양점에서 등심 고기 사실 분?"
+            value={formData.title}
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="mb-6">
+          <div>
+            <label htmlFor="province" className="mb-3 block text-[#1F2937]">
+              어디서 만날까요??
+            </label>
+            <div className="mb-3 flex items-center gap-2">
+              <select
+                name="province"
+                id="province"
+                required
+                value={formData.province}
+                className="min-w-[120px] cursor-pointer appearance-none rounded-md border-2 border-[#f3f5f6] bg-white px-3 py-2 pr-8 text-gray-700 transition-all duration-200 hover:border-gray-500 focus:border-gray-500 focus:outline-none"
+                onChange={(e) =>
+                  setFormData({ ...formData, province: e.target.value })
+                }
+              >
+                {modelProvinceOptions.map((province) => (
+                  <option key={province.value} value={province.value}>
+                    {province.label}
+                  </option>
+                ))}
+              </select>
+              <select
+                name="city"
+                id="city"
+                required
+                value={formData.city}
+                className="min-w-[120px] cursor-pointer appearance-none rounded-md border-2 border-[#f3f5f6] bg-white px-3 py-2 pr-8 text-gray-700 transition-all duration-200 hover:border-gray-500 focus:border-gray-500 focus:outline-none"
+                onChange={(e) =>
+                  setFormData({ ...formData, city: e.target.value })
+                }
+              >
+                {modelCityOptions.map((city) => (
+                  <option key={city.value} value={city.value}>
+                    {city.label}
+                  </option>
+                ))}
+              </select>
+              <select
+                name="district"
+                id="district"
+                required
+                value={formData.district}
+                className="min-w-[120px] cursor-pointer appearance-none rounded-md border-2 border-[#f3f5f6] bg-white px-3 py-2 pr-8 text-gray-700 transition-all duration-200 hover:border-gray-500 focus:border-gray-500 focus:outline-none"
+                onChange={(e) =>
+                  setFormData({ ...formData, district: e.target.value })
+                }
+              >
+                {modelDistrictOptions.map((district) => (
+                  <option key={district.value} value={district.value}>
+                    {district.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="mb-6">
+          <TextInput
+            name="detail"
+            required
+            id="detail"
+            placeholder="나머지 장소를 입력해주세요"
+            value={formData.detail}
+            onChange={(e) =>
+              setFormData({ ...formData, detail: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="date" className="mb-3 block">
+            모임의 설명글을 작성해보세요!
+          </label>
+          <Textarea
+            className="min-h-[173px]"
+            name="description"
+            id="description"
+            required
+            value={formData.description}
+            placeholder={`모집 내용을 작성해주세요.
+
+ex) 대량 고기를 사서 나누고 싶어요.
+그 외 필요한 구매 물품은 개인 구매하셔도 되어요.
+함께 장보기 할 인원은 3명 정도 생각하고 있어요.이번주 토요일인 10월 10일  3시에 만나기로 해요!`}
+            onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+          />
+        </div>
+
+        <Button
+          label="확인"
+          type="submit"
+          className={`w-full border-none text-white ${
+            isValid ? '!bg-primary' : '!bg-gray-30'
+          }`}
+          disabled={!isValid}
+        />
+      </form>
+    </>
+  );
+}
--- src/constants/locations.js
+++ src/constants/locations.js
@@ -17,3 +17,39 @@ export const cityOptions = [
   { value: '강서구', label: '강서구' },
   { value: '강북구', label: '강북구' },
 ];
+
+export const districtOptions = [
+  { value: '', label: '전체' },
+  { value: '장곡동', label: '장곡동' },
+  { value: '원당동', label: '원당동' },
+  { value: '대장동', label: '대장동' },
+  { value: '문충동', label: '문충동' },
+];
+
+export const modelProvinceOptions = [
+  { value: '', label: '지역 선택' },
+  { value: '서울특별시', label: '서울특별시' },
+  { value: '경기도', label: '경기도' },
+  { value: '인천시', label: '인천시' },
+  { value: '강원도', label: '강원도' },
+  { value: '충청도', label: '충청도' },
+  { value: '전라도', label: '전라도' },
+  { value: '경상도', label: '경상도' },
+  { value: '제주도', label: '제주도' },
+];
+
+export const modelCityOptions = [
+  { value: '', label: '시/군/구 선택' },
+  { value: '강남구', label: '강남구' },
+  { value: '강동구', label: '강동구' },
+  { value: '강서구', label: '강서구' },
+  { value: '강북구', label: '강북구' },
+];
+
+export const modelDistrictOptions = [
+  { value: '', label: '동/읍/면 선택' },
+  { value: '장곡동', label: '장곡동' },
+  { value: '원당동', label: '원당동' },
+  { value: '대장동', label: '대장동' },
+  { value: '문충동', label: '문충동' },
+];
