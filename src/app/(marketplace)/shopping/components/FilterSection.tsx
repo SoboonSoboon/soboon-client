4fe7c3d22f36e42129150b238433027bbdcf6bfe
kim dong hyeon <kdh990315@naver.com>
2025-10-15 20:31:07 +0900

    feat: 필터 구현

--- src/app/(marketplace)/shopping/components/FilterSection.tsx
+++ src/app/(marketplace)/shopping/components/FilterSection.tsx
@@ -1,35 +1,107 @@
-import { DateFilter, Dropdown } from '@/components';
+'use client';
+
+import { DateFilter } from '@/components';
+import { cityOptions, provinceOptions } from '@/constants/locations';
+import { ChevronDown } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
 
 export const FilterSection = () => {
-  const cityOptions = [
-    { value: '1', label: '서울특별시' },
-    { value: '2', label: '경기도' },
-    { value: '3', label: '인천시' },
-    { value: '4', label: '강원도' },
-    { value: '5', label: '충청도' },
-    { value: '6', label: '전라도' },
-    { value: '7', label: '경상도' },
-    { value: '8', label: '제주도' },
-  ];
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-  const districtOptions = [
-    { value: '1', label: '강남구' },
-    { value: '2', label: '강동구' },
-    { value: '3', label: '강서구' },
-    { value: '4', label: '강북구' },
-    { value: '5', label: '강서구' },
-    { value: '6', label: '강서구' },
-    { value: '7', label: '강서구' },
-  ];
+  const activeProvince = searchParams.get('province') || '';
+  const activeCity = searchParams.get('city') || '';
+
+  const handleRegionChange = useCallback(
+    (province: string, city: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+
+      if (province === '') {
+        params.delete('province');
+      } else {
+        params.set('province', province);
+      }
+
+      if (city === '') {
+        params.delete('city');
+      } else {
+        params.set('city', city);
+      }
+
+      requestAnimationFrame(() => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      });
+
+      router.push(`?${params.toString()}`);
+    },
+    [router, searchParams],
+  );
+
+  const handleDateChange = (value: 'RECENT' | 'OLDEST') => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('sortType', value);
+    router.push(`?${params.toString()}`);
+  };
 
   return (
     <div className="mb-8 flex justify-between">
       <div className="flex gap-4">
-        <Dropdown options={cityOptions} />
-        <Dropdown options={districtOptions} />
+        <div className="relative">
+          <select
+            name=""
+            id=""
+            className="min-w-[120px] cursor-pointer appearance-none rounded-md border-2 border-[#f3f5f6] bg-white px-3 py-2 pr-8 text-gray-700 transition-all duration-200 hover:border-gray-500 focus:border-gray-500 focus:outline-none"
+            onChange={(e) => {
+              const selectedProvince = provinceOptions.find(
+                (city) => city.value === e.target.value,
+              );
+              handleRegionChange(selectedProvince?.value || '', activeCity);
+              if (selectedProvince?.value === '') {
+                handleRegionChange('', '');
+              }
+            }}
+            value={activeProvince}
+          >
+            {provinceOptions.map((city) => (
+              <option key={city.value} value={city.value}>
+                {city.label}
+              </option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronDown className="h-4 w-4 text-gray-500 transition-transform duration-200 group-hover:rotate-180" />
+          </div>
+        </div>
+        <div className="relative">
+          <select
+            name=""
+            id=""
+            className="min-w-[120px] cursor-pointer appearance-none rounded-md border-2 border-[#f3f5f6] bg-white px-3 py-2 pr-8 text-gray-700 transition-all duration-200 hover:border-gray-500 focus:border-gray-500 focus:outline-none"
+            onChange={(e) => {
+              const selectedCity = cityOptions.find(
+                (district) => district.value === e.target.value,
+              );
+              handleRegionChange(activeProvince, selectedCity?.value || '');
+            }}
+            value={activeCity}
+          >
+            {cityOptions.map((district) => (
+              <option key={district.value} value={district.value}>
+                {district.label}
+              </option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronDown className="h-4 w-4 text-gray-500 transition-transform duration-200 group-hover:rotate-180" />
+          </div>
+        </div>
       </div>
       <div>
-        <DateFilter />
+        <DateFilter onChange={(value) => handleDateChange(value)} />
       </div>
     </div>
   );
