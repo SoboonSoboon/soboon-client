53d38711f05292624bfce4820e0658a0acfc71be
misung.dev <misung.dev@gmail.com>
2025-10-17 09:59:03 +0900

    style: hover 효과 적용

--- src/components/Atoms/UserMenu/UserMenu.tsx
+++ src/components/Atoms/UserMenu/UserMenu.tsx
@@ -15,12 +15,12 @@ export const UserMenu = ({ className, ...props }: UserMenuProps) => {
       {...props}
     >
       <ul className="text-gray-90 flex flex-col">
-        <li className="flex border-b border-[var(--GrayScale-Gray10)] px-4 py-2.5 text-[#1F2937]">
+        <li className="hover:bg-orange-5 flex cursor-pointer border-b border-[var(--GrayScale-Gray10)] px-4 py-2.5 text-[#1F2937] transition-all duration-200 hover:rounded-t-xl">
           <Link href="/mypage">
             <span>마이페이지</span>
           </Link>
         </li>
-        <li className="flex cursor-pointer px-4 py-2.5">
+        <li className="hover:bg-orange-5 flex cursor-pointer px-4 py-2.5 transition-all duration-200 hover:rounded-b-xl">
           <span>로그아웃</span>
         </li>
       </ul>
