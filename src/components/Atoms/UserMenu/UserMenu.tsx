c89ea6ff52f45eb1fa9feaffe0fe0bfcd24bf361
misung.dev <misung.dev@gmail.com>
2025-10-17 10:04:42 +0900

    feat: 모달 바깥 클릭하면 모달 닫히는 기능 추가

--- src/components/Atoms/UserMenu/UserMenu.tsx
+++ src/components/Atoms/UserMenu/UserMenu.tsx
@@ -1,13 +1,33 @@
 import { cn } from '@/utils/cn';
 import Link from 'next/link';
+import { useEffect, useRef } from 'react';
 
 export interface UserMenuProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
+  onClose?: () => void;
 }
 
-export const UserMenu = ({ className, ...props }: UserMenuProps) => {
+export const UserMenu = ({ className, onClose, ...props }: UserMenuProps) => {
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        onClose?.();
+      }
+    };
+
+    if (onClose) {
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }
+  }, [onClose]);
+
   return (
     <div
+      ref={menuRef}
       className={cn(
         'border-gray-10 flex w-35 flex-col rounded-xl border-1 bg-white',
         className,
