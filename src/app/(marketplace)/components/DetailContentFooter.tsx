4b41a8a1d535896face1dd43527b44d41eb3f55a
kim dong hyeon <kdh990315@naver.com>
2025-10-17 11:12:25 +0900

    fix: 조회수 삭제

--- src/app/(marketplace)/components/DetailContentFooter.tsx
+++ src/app/(marketplace)/components/DetailContentFooter.tsx
@@ -4,7 +4,6 @@ export const DetailContentFooter = ({ createdAt }: { createdAt: string }) => {
   return (
     <div className="text-text-sub2 mt-8 flex justify-end gap-4">
       <p>{timeFormatter(createdAt)}</p>
-      <p>조회 4,938</p>
     </div>
   );
 };
