5787c431a5009b9c633f253aaee61bb08aa464ea
keeprok <140785214+keeprok@users.noreply.github.com>
2025-10-20 01:02:17 +0900

    refactor: button 디자인 추가 및  cn 정리

--- src/components/Atoms/Button/Button.tsx
+++ src/components/Atoms/Button/Button.tsx
@@ -1,6 +1,9 @@
+import { cn } from '@/utils/cn';
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   primary?: boolean;
+  variant?: 'primary' | 'secondary' | 'outline' | 'block';
   backgroundColor?: string;
   size?: 'small' | 'medium' | 'large';
   label: string;
@@ -8,23 +11,30 @@ export interface ButtonProps
 
 export const Button = ({
   primary = false,
+  variant,
   size = 'medium',
   backgroundColor,
   label,
   className,
   ...props
 }: ButtonProps) => {
+  // variant가 없으면 기존 primary prop 사용 (mvp 끝나고 나서 common리팩토링)
+  const buttonVariant = variant || (primary ? 'primary' : 'secondary');
+
   const baseStyles =
-    'inline-block cursor-pointer border-1 rounded-[8px] leading-none';
+    'inline-block cursor-pointer rounded-[8px] border-1 leading-none';
 
-  // Primary/Secondary styles
-  const modeStyles = primary
-    ? 'bg-primary text-white min-w-[115px]'
-    : 'shadow-[rgba(0,0,0,0.15)_0px_0px_0px_1px_inset] bg-transparent text-[#333]';
+  const variantStyles = {
+    primary: 'bg-primary min-w-[115px] text-white',
+    secondary:
+      'bg-transparent text-[#333] shadow-[rgba(0,0,0,0.15)_0px_0px_0px_1px_inset]',
+    outline:
+      'text-primary border-primary bg-white hover:bg-primary hover:text-white',
+    block: 'bg-gray-40 text-gray-80 cursor-none border-none ',
+  };
 
-  // Size styles
   const sizeStyles = {
-    small: 'px-4 py-2.5 text-base ',
+    small: 'px-4 py-2.5 text-base',
     medium: 'px-5 py-[11px] text-base',
     large: 'px-6 py-3 text-base',
   };
@@ -32,7 +42,12 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={`${baseStyles} ${modeStyles} ${sizeStyles[size]} ${className}`}
+      className={cn(
+        baseStyles,
+        variantStyles[buttonVariant],
+        sizeStyles[size],
+        className,
+      )}
       style={backgroundColor ? { backgroundColor } : undefined}
       {...props}
     >
