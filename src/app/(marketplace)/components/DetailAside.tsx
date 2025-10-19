7e6bafd8e728ef7374d8cb3f003be14c07e3d7b2
misung.dev <misung.dev@gmail.com>
2025-10-20 01:21:03 +0900

    feat: StatusTag 컴포넌트 추가 및 gap으로 간격 조정

--- src/app/(marketplace)/components/DetailAside.tsx
+++ src/app/(marketplace)/components/DetailAside.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { Button } from '@/components';
+import { Button, StatusTag } from '@/components';
 import { EllipsisVertical, MapPin, Bookmark } from 'lucide-react';
 import { useState, useRef } from 'react';
 import { ActionMenu } from './ActionMenu/ActionMenu';
@@ -23,10 +23,12 @@ export const DetailAside = ({
   const buttonRef = useRef<HTMLDivElement>(null);
 
   return (
-    <aside className="w-[430px]">
+    <aside className="flex w-[430px] flex-col gap-5">
       <div className="flex w-full justify-between">
         {/* 상태 바 */}
-        <div>모집중</div>
+        <div>
+          <StatusTag status="RECRUITING" />
+        </div>
 
         {/* 아이콘 버튼 */}
         <div className="relative flex cursor-pointer justify-center">
@@ -54,10 +56,8 @@ export const DetailAside = ({
         </div>
       </div>
 
-      <div>
-        <h2 className="font-memomentKkukkkuk mb-2 line-clamp-2 text-2xl">
-          {title}
-        </h2>
+      <div className="flex flex-col gap-3">
+        <h2 className="font-memomentKkukkkuk line-clamp-2 text-2xl">{title}</h2>
         <div className="flex items-center gap-2">
           {/* 추후 공용 컴포넌트 수정 후 교체 예정 */}
           <Image
@@ -71,9 +71,9 @@ export const DetailAside = ({
         </div>
       </div>
 
-      <div className="bg-gray-10 my-5 h-[1px] w-full"></div>
+      <div className="bg-gray-10 h-[1px] w-full"></div>
 
-      <div className="mb-5 flex w-full justify-between">
+      <div className="flex w-full justify-between">
         <div className="flex items-center gap-1">
           <MapPin className="size-6" />
           <p>{detail_address}</p>
