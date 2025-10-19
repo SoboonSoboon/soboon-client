a32854cacc5ef5f5bf8c71393dfbd314ddf0d42b
kim dong hyeon <kdh990315@naver.com>
2025-10-20 03:40:33 +0900

    feat: 대댓글 등록 구현

--- src/action/createComment.ts
+++ src/action/createComment.ts
@@ -32,3 +32,35 @@ export const createComment = async (_: unknown, formData: FormData) => {
     return null;
   }
 };
+
+export const createReply = async (_: unknown, formData: FormData) => {
+  const meetingId = formData.get('meetingId') as string;
+  const commentId = formData.get('commentId') as string;
+  const replyContent = formData.get('reply') as string;
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SOBOON_API_URL}/v1/meetings/${meetingId}/comments/${commentId}/replies`,
+      {
+        method: 'POST',
+        body: JSON.stringify({ content: replyContent, secret: false }),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_SOBOON_API_TOKEN}`,
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error('대댓글 생성 실패');
+    }
+
+    revalidatePath(`/sharing/${meetingId}`);
+
+    const responseData = await response.json();
+    return responseData.message;
+  } catch (error) {
+    console.error('대댓글 생성 실패', error);
+    return null;
+  }
+};
