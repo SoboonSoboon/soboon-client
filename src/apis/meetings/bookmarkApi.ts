c192508a5df958526cabe2e0f9923958a0835542
misung.dev <misung.dev@gmail.com>
2025-10-21 14:31:41 +0900

    remove: 중복된 API 함수 삭제

--- src/apis/meetings/bookmarkApi.ts
+++ src/apis/meetings/bookmarkApi.ts
@@ -1,10 +1,5 @@
 import { axiosInstance } from '../axiosInstance';
 
-export const getBookmarkedMeetingListApi = async () => {
-  const response = await axiosInstance.get('/v1/me/bookmarks');
-  return response.data;
-};
-
 export const postBookmarkedMeetingApi = async (id: number) => {
   const response = await axiosInstance.post(`/v1/meetings/${id}/bookmarks`);
   return response.data;
