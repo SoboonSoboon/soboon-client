40618cc963000a41f19c0a8c1e0827c07842bff0
kim dong hyeon <kdh990315@naver.com>
2025-10-22 03:20:40 +0900

    feat: 댓글 디자인 수정

--- src/app/(marketplace)/components/comment/CommentItem.tsx
+++ src/app/(marketplace)/components/comment/CommentItem.tsx
@@ -10,22 +10,25 @@ export const CommentItem = ({
 }) => {
   return (
     <div>
-      <div className="flex gap-3">
-        <ProfileImg profileImageUrl={comment.authorProfileImageUrl} size={32} />
-        <div className="flex-1 gap-2">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <span className="text-text-main font-bold">
-                {comment.authorNickname}
-              </span>
-              <span className="text-sm text-gray-50">
-                {timeFormatter(comment.createdAt)}
-              </span>
-            </div>
-            <div className="cursor-pointer">
-              <EllipsisVertical className="size-6 text-gray-50" />
-            </div>
+      <div className="">
+        <div className="flex justify-between">
+          <div className="mb-2 flex items-center gap-2">
+            <ProfileImg
+              profileImageUrl={comment.authorProfileImageUrl}
+              size={32}
+            />
+            <span className="text-text-main font-bold">
+              {comment.authorNickname}
+            </span>
+            <span className="text-sm text-gray-50">
+              {timeFormatter(comment.createdAt)}
+            </span>
           </div>
+          <div className="cursor-pointer">
+            <EllipsisVertical className="size-6 text-gray-50" />
+          </div>
+        </div>
+        <div>
           <p className="text-gray-95">{comment.content}</p>
         </div>
       </div>
