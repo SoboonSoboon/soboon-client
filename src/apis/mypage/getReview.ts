2278d5431bff99f607667629859ad05da08e3c5a
keeprok <140785214+keeprok@users.noreply.github.com>
2025-10-22 11:08:19 +0900

    fix: 환경변수 에러처리

--- src/apis/mypage/getReview.ts
+++ src/apis/mypage/getReview.ts
@@ -1,21 +1,28 @@
 import { ReviewResponse } from '@/utils/review';
 
-export async function getReceivedReview(): Promise<ReviewResponse> {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_SOBOON_API_URL}/v1/reviews/me`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_SOBOON_API_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-      cache: 'force-cache',
-      next: { revalidate: 300 },
+export const getReceivedReview = async (): Promise<ReviewResponse> => {
+  const baseUrl = process.env.NEXT_PUBLIC_SOBOON_API_URL;
+  const token = process.env.NEXT_PUBLIC_SOBOON_API_TOKEN;
+  if (!baseUrl) {
+    throw new Error('NEXT_PUBLIC_SOBOON_API_URL is not defined');
+  }
+
+  if (!token) {
+    throw new Error('NEXT_PUBLIC_SOBOON_API_TOKEN is not defined');
+  }
+
+  const response = await fetch(`${baseUrl}/v1/reviews/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
     },
-  );
+    cache: 'force-cache',
+    next: { revalidate: 300 },
+  });
 
   if (!response.ok) {
     throw new Error('Failed to fetch reviews');
   }
 
   return response.json();
-}
+};
