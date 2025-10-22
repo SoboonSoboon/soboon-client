3d483abc99802fd53e28dffc348671cfd7fdb599
misung.dev <misung.dev@gmail.com>
2025-10-22 23:44:52 +0900

    style: 버튼 컴포넌트 수정 및 지원 버튼 섹션에 적용

--- src/components/Atoms/Button/Button.tsx
+++ src/components/Atoms/Button/Button.tsx
@@ -6,6 +6,7 @@ export interface ButtonProps
   variant?: 'filled' | 'outline';
   size?: 'small' | 'large';
   label?: string;
+  children?: React.ReactNode;
 }
 
 export const Button = ({
@@ -16,28 +17,29 @@ export const Button = ({
   variant = 'filled',
   className,
   label,
+  children,
   ...props
 }: ButtonProps) => {
   const baseStyles =
-    'cursor-pointer inline-flex items-center justify-center rounded-lg border font-medium select-none transition h-11';
+    'inline-flex items-center justify-center rounded-lg border font-medium select-none transition h-11';
 
   const sizes = {
-    small: 'h-10 px-4 text-sm', // 모바일 사이즈
-    large: 'h-11 px-5 text-base', // 데스크탑 사이즈
+    small: 'h-10 w-[120px] px-4 text-sm', // 모바일 사이즈
+    large: 'h-11 w-[332px] px-5 text-base', // 데스크탑 사이즈
   } as const;
 
   const variants = {
     filled:
-      'bg-primary text-white border-transparent hover:bg-[var(--GreenScale-Green60)] active:bg-[var(--GreenScale-Green70)]',
+      'cursor-pointer bg-primary text-white border-transparent hover:bg-[var(--GreenScale-Green60)] active:bg-[var(--GreenScale-Green70)]',
     outline:
-      'bg-white text-primary border-primary active:border-[var(--GreenScale-Green70)] active:text-[var(--GreenScale-Green70)]',
+      'cursor-pointer bg-white text-primary border-primary active:border-[var(--GreenScale-Green70)] active:text-[var(--GreenScale-Green70)]',
   } as const;
 
   const disabledVariants = {
     filled:
-      'bg-[var(--GrayScale-Gray20)] text-white border-transparent cursor-not-allowed',
+      'bg-[var(--GrayScale-Gray30)] text-white border-transparent cursor-not-allowed font-normal',
     outline:
-      'bg-white text-[var(--GrayScale-Gray30)] border-[var(--GrayScale-Gray40)] cursor-not-allowed',
+      'bg-white text-[var(--GrayScale-Gray30)] border-[var(--GrayScale-Gray40)] cursor-not-allowed font-normal',
   } as const;
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -62,7 +64,7 @@ export const Button = ({
       )}
       {...props}
     >
-      {label}
+      {children || label}
     </button>
   );
 };
