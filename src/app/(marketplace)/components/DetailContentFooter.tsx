f5b677df94c879eebe5937bb8d57337dafc29e4d
misung.dev <misung.dev@gmail.com>
2025-10-23 18:09:46 +0900

    style: 작성됨 문구 추가

--- src/app/(marketplace)/components/DetailContentFooter.tsx
+++ src/app/(marketplace)/components/DetailContentFooter.tsx
@@ -3,7 +3,7 @@ import { timeFormatter } from '@/utils/timeFormetter';
 export const DetailContentFooter = ({ createdAt }: { createdAt: string }) => {
   return (
     <div className="text-text-sub2 mt-8 flex justify-end gap-4">
-      <p>{timeFormatter(createdAt)}</p>
+      <p>{timeFormatter(createdAt)} 작성됨</p>
     </div>
   );
 };
