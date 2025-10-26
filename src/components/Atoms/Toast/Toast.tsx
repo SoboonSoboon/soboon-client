fce0ab0bce710bfa25cc4c33e766f150447aac0d
kim dong hyeon <kdh990315@naver.com>
2025-10-27 02:58:59 +0900

    fix: 불필요한 코드 삭제

--- src/components/Atoms/Toast/Toast.tsx
+++ src/components/Atoms/Toast/Toast.tsx
@@ -6,14 +6,6 @@ interface ToastProps {
   remove: (id: string) => void;
 }
 
-const styles = {
-  default: 'bg-white border-gray-200 text-gray-900',
-  error: 'bg-red-50 border-red-200 text-red-900',
-  success: 'bg-green-50 border-green-200 text-green-900',
-  warning: 'bg-yellow-50 border-yellow-200 text-yellow-900',
-  info: 'bg-blue-50 border-blue-200 text-blue-900',
-};
-
 const icons = {
   default: (
     <>
