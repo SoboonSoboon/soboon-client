43efeb6e6925cad1d1c0d7066442cb897dda43e7
keeprok <140785214+keeprok@users.noreply.github.com>
2025-10-27 17:08:07 +0900

    fix: 위치 조정

--- src/components/Molecules/ErrorPage/Errorpage.tsx
+++ src/components/Molecules/ErrorPage/Errorpage.tsx
@@ -5,7 +5,7 @@ export const ErrorPage = () => {
   const onButtonClick = () => (window.location.href = '/');
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-5">
+    <div className="flex flex-col items-center justify-center gap-5 py-20">
       <Image
         src="/images/error_image.svg"
         alt="에러 이미지"
@@ -14,7 +14,7 @@ export const ErrorPage = () => {
         className="mx-auto"
       />
 
-      <p className="text-gray-80 mb-8 text-xl leading-tight font-semibold">
+      <p className="leading-tight font-semibold">
         요청하신 페이지를 찾을 수 없습니다.
         <br />
         입력하신 주소가 올바른지 확인해주세요.
