5e5a5d4989866fffd88f4d0e331a192efe0c4c34
misung.dev <misung.dev@gmail.com>
2025-10-27 16:04:48 +0900

    feat: 헤더 반응형 구현

--- src/components/Molecules/Header/Header.tsx
+++ src/components/Molecules/Header/Header.tsx
@@ -38,12 +38,12 @@ export const Header = () => {
   return (
     <header className="border-gray-10 h-15 border-b bg-white px-4 dark:bg-black">
       <div className="text-text-main mx-auto flex h-full max-w-[1200px] items-center justify-between bg-white dark:bg-black dark:text-white">
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-4 sm:gap-6 md:gap-8 lg:gap-10">
           <Link href="/">
             <Logo />
           </Link>
 
-          <nav className="flex items-center gap-6 text-base font-normal">
+          <nav className="flex items-center gap-3 text-base font-normal sm:gap-4 md:gap-5 lg:gap-6">
             {isLoggedIn && (
               <>
                 {Object.entries(HEADER_MENU).map(([key, headerMenu]) => (
@@ -54,7 +54,7 @@ export const Header = () => {
                       pathname.startsWith(headerMenu.PATH) ? 'text-primary' : ''
                     }`}
                   >
-                    <div className="relative">
+                    <div className="relative hidden sm:flex sm:items-center">
                       <Icon
                         type={
                           pathname.startsWith(headerMenu.PATH)
@@ -70,7 +70,7 @@ export const Header = () => {
                         className="absolute top-0 left-0 opacity-0 transition-opacity duration-150 ease-in-out group-hover:opacity-100"
                       />
                     </div>
-                    <span className="font-memomentKkukkkuk">
+                    <span className="font-memomentKkukkkuk leading-none">
                       {headerMenu.LABEL}
                     </span>
                   </Link>
@@ -80,12 +80,12 @@ export const Header = () => {
           </nav>
         </div>
 
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-2 sm:gap-3 md:gap-4 lg:gap-5">
           {isLoggedIn ? (
             <>
               <div className="relative">
                 <span
-                  className="flex items-center gap-[10px]"
+                  className="flex items-center gap-1 sm:gap-2 md:gap-[10px]"
                   onClick={() => {
                     setIsOpen(true);
                   }}
