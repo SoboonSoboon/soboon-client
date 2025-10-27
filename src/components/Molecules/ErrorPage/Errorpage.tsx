0132989cb2eba82ca69f882f12e9b33fcd589416
keeprok <140785214+keeprok@users.noreply.github.com>
2025-10-27 17:02:18 +0900

    design: errorPage

---
+++ src/components/Molecules/ErrorPage/Errorpage.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@/components';
+import Image from 'next/image';
+
+export const ErrorPage = () => {
+  const onButtonClick = () => (window.location.href = '/');
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-5">
+      <Image
+        src="/images/error_image.svg"
+        alt="에러 이미지"
+        width={200}
+        height={200}
+        className="mx-auto"
+      />
+
+      <p className="text-gray-80 mb-8 text-xl leading-tight font-semibold">
+        요청하신 페이지를 찾을 수 없습니다.
+        <br />
+        입력하신 주소가 올바른지 확인해주세요.
+      </p>
+      <Button label="홈으로" onClick={onButtonClick} variant="outline" />
+    </div>
+  );
+};
