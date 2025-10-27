43424726c9bd54d07a55950731a28bb5a630413d
misung.dev <misung.dev@gmail.com>
2025-10-27 13:22:30 +0900

    style: footer 수정

--- src/components/Atoms/Footer/Footer.tsx
+++ src/components/Atoms/Footer/Footer.tsx
@@ -1,26 +1,11 @@
 'use client';
 
-import Link from 'next/link';
-import { Logo } from '../Logo';
-
 export const Footer = () => {
   return (
-    <footer className="mt-10 flex h-60.5 w-full items-center bg-[var(--GrayScale-Gray80)]">
-      <div className="mx-auto flex w-[1200px] flex-col gap-4.5 text-[var(--GrayScale-Gray5)]">
-        <Link href="/">
-          <Logo width={75} height={28} className="text-white" />
-        </Link>
-        <div className="flex flex-col gap-2">
-          <div>
-            <strong>팀 소분소분 | </strong>
-            <span>이영록 김동현 류미성 최지희 박성민 이정우 김나래</span>
-          </div>
-          <div>
-            <strong>이메일 | </strong>
-            <span>soboonsoboon@gmail.com</span>
-          </div>
-        </div>
-      </div>
+    <footer className="border-gray-10 mt-10 flex w-full items-center justify-center border-t bg-white py-8">
+      <span className="text-gray-70 py-2.5">
+        ⓒ Soboon. All rights reserved.
+      </span>
     </footer>
   );
 };
