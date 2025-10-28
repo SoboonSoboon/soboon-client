1d1d30f1e9e207099acc17150f3cb4ad7dfff982
kdh990315 <104114383+kdh990315@users.noreply.github.com>
2025-10-28 23:30:07 +0900

    fix: 디자인 QA반영 (#279)
    * feat: 댓글 수정 완료 및 실패 시 메시지 수정
    
    * feat: 댓글 수정에도 비밀 댓글 기능 추가
    
    * feat: 자물쇠 아이콘 추가
    
    * design: 자물쇠 아이콘과 댓글의 높이 맞추기
    
    * design: 신청자 버튼 디자인 수정
    
    * fix: 참여자가 1명 이하일 때 마감 못하게 막기
    
    * fix: 모집 마감일때 신청 버튼 비황성화
    
    * feat: 모임 마감시 참여자가 신청자 승인 목록을 볼 수 있도록 수정

--- src/components/marketplace/DetailAside/ApplyStatusButtonSection.tsx
+++ src/components/marketplace/DetailAside/ApplyStatusButtonSection.tsx
@@ -2,31 +2,37 @@
 
 import { UserApplyStatusType } from '@/apis/meetings/userApplyStatusApi';
 import { Button } from '@/components';
+import { StatusString } from '@/types/common';
 
 export const ApplyStatusButtonSection = ({
   filteredStatus,
   handleApplyMeeting,
   handleCancelApplyMeeting,
   meetingId,
+  status,
 }: {
   filteredStatus: UserApplyStatusType | null | undefined;
   handleApplyMeeting: (meetingId: string) => void;
   handleCancelApplyMeeting: (meetingId: string) => void;
   meetingId: string;
+  status: StatusString;
 }) => {
+  const isCompletedOrClosed = status === 'COMPLETED' || status === 'CLOSED';
+
   return (
     <>
       <div className="mb-5">
         {/* 모임 신청이 가능한 버튼 */}
-        {(!filteredStatus?.participationStatus ||
-          filteredStatus?.participationStatus === 'CANCELLED') && (
-          <Button
-            label="모임 신청"
-            className="w-full"
-            variant="filled"
-            onClick={() => handleApplyMeeting(meetingId)}
-          />
-        )}
+        {!isCompletedOrClosed &&
+          (!filteredStatus?.participationStatus ||
+            filteredStatus?.participationStatus === 'CANCELLED') && (
+            <Button
+              label="모임 신청"
+              className="w-full"
+              variant="filled"
+              onClick={() => handleApplyMeeting(meetingId)}
+            />
+          )}
 
         {/* 모임 신청 후 취소가 가능한 버튼 */}
         {filteredStatus?.participationStatus === 'APPLIED' && (
@@ -62,6 +68,15 @@ export const ApplyStatusButtonSection = ({
         {filteredStatus?.participationStatus === 'KICKED' && (
           <Button
             label="모임에서 강퇴되었어요."
+            variant="filled"
+            className="w-full !bg-[var(--GrayScale-Gray30)]"
+            disabled
+          />
+        )}
+
+        {isCompletedOrClosed && !filteredStatus?.participationStatus && (
+          <Button
+            label="모집 완료"
             variant="filled"
             className="w-full !bg-[var(--GrayScale-Gray30)]"
             disabled
