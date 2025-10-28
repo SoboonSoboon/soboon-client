6d9adbca97b062dd677a5ca9e82b3ddf24d94db7
keeprok <140785214+keeprok@users.noreply.github.com>
2025-10-28 10:45:10 +0900

    feat: popoverReview연결

--- src/apis/meetings/getPopoverReview.ts
+++ src/apis/meetings/getPopoverReview.ts
@@ -1,7 +1,9 @@
 import { axiosInstance } from '../axiosInstance';
 import { ReviewResponse } from '@/app/mypage/utils/review';
 
-export const getPopReview = async (userId: number): Promise<ReviewResponse> => {
+export const getPopoverReview = async (
+  userId: number,
+): Promise<ReviewResponse> => {
   try {
     const response = await axiosInstance.get<ReviewResponse>(
       `/v1/reviews/${userId}`,
---
+++ src/hooks/usePopoverReview.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import { getPopoverReview } from '@/apis/meetings/getPopoverReview';
+import { ReviewResponse } from '@/app/mypage/utils/review';
+
+export const usePopoverReview = (userId: number) => {
+  return useQuery<ReviewResponse>({
+    queryKey: ['popReview', userId],
+    queryFn: () => getPopoverReview(userId),
+    staleTime: 5 * 60 * 1000, // 5분간 캐시 유지
+  });
+};
