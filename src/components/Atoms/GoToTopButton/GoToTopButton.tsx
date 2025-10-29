ba49a2ee393f2fad19a2ca0adba1127bf62afb0b
misung.dev <misung.dev@gmail.com>
2025-10-29 16:47:15 +0900

    style: border-1 >> border로 변경

--- src/components/Atoms/GoToTopButton/GoToTopButton.tsx
+++ src/components/Atoms/GoToTopButton/GoToTopButton.tsx
@@ -10,7 +10,7 @@ export const GoToTopButton = () => {
   return (
     <div className="flex w-16.5 cursor-pointer flex-col items-center gap-1 px-3 py-1">
       <div
-        className="text-text-line2 border-text-line1 flex size-11 flex-col items-center justify-center rounded-full border-1 bg-white"
+        className="text-text-line2 border-text-line1 flex size-11 flex-col items-center justify-center rounded-full border bg-white"
         onClick={handleClick}
         data-testid="go-to-top-button"
       >
