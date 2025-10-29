8f6ca6b92453c49945f913e6d205f8879f2df096
misung.dev <misung.dev@gmail.com>
2025-10-29 23:58:44 +0900

    fix: 지역 수정

--- src/constants/locations.js
+++ src/constants/locations.js
@@ -16,23 +16,24 @@ export const MODEL_PROVINCE_OPTIONS = [
 export const CITY_OPTIONS_BY_PROVINCE = {
   서울특별시: [
     { value: '강남구', label: '강남구' },
+    { value: '강동구', label: '강동구' },
+    { value: '광진구', label: '광진구' },
     { value: '마포구', label: '마포구' },
     { value: '서대문구', label: '서대문구' },
     { value: '서초구', label: '서초구' },
     { value: '용산구', label: '용산구' },
     { value: '종로구', label: '종로구' },
     { value: '중구', label: '중구' },
-    { value: '강동구', label: '강동구' },
   ],
   경기도: [
     { value: '고양시', label: '고양시' },
+    { value: '남양주시', label: '남양주시' },
     { value: '성남시', label: '성남시' },
     { value: '수원시', label: '수원시' },
+    { value: '시흥시', label: '시흥시' },
     { value: '파주시', label: '파주시' },
     { value: '평택시', label: '평택시' },
     { value: '화성시', label: '화성시' },
-    { value: '시흥시', label: '시흥시' },
-    { value: '남양주시', label: '남양주시' },
   ],
   인천광역시: [
     { value: '계양구', label: '계양구' },
@@ -74,9 +75,9 @@ export const DISTRICT_OPTIONS_BY_CITY = {
   ],
   용산구: [
     { value: '남영동', label: '남영동' },
-    { value: '한남동', label: '한남동' },
-    { value: '이태원동', label: '이태원동' },
     { value: '용산동', label: '용산동' },
+    { value: '이태원동', label: '이태원동' },
+    { value: '한남동', label: '한남동' },
   ],
   종로구: [
     { value: '사직동', label: '사직동' },
@@ -96,14 +97,20 @@ export const DISTRICT_OPTIONS_BY_CITY = {
     { value: '성내동', label: '성내동' },
     { value: '천호동', label: '천호동' },
     { value: '풍납동', label: '풍납동' },
-    { value: '천호동', label: '천호동' },
+  ],
+  광진구: [
+    { value: '광장동', label: '광장동' },
+    { value: '구의동', label: '구의동' },
+    { value: '능동', label: '능동' },
+    { value: '중곡동', label: '중곡동' },
+    { value: '화양동', label: '화양동' },
   ],
   고양시: [
+    { value: '덕양구', label: '덕양구' },
     { value: '일산동구', label: '일산동구' },
     { value: '일산서구', label: '일산서구' },
-    { value: '덕양구', label: '덕양구' },
-    { value: '화정동', label: '화정동' },
     { value: '주엽동', label: '주엽동' },
+    { value: '화정동', label: '화정동' },
   ],
   성남시: [
     { value: '분당구', label: '분당구' },
@@ -111,24 +118,22 @@ export const DISTRICT_OPTIONS_BY_CITY = {
     { value: '중원구', label: '중원구' },
   ],
   수원시: [
+    { value: '권선구', label: '권선구' },
     { value: '영통구', label: '영통구' },
     { value: '장안구', label: '장안구' },
-    { value: '권선구', label: '권선구' },
     { value: '팔달구', label: '팔달구' },
   ],
   파주시: [
     { value: '문발동', label: '문발동' },
-    { value: '파주읍', label: '파주읍' },
     { value: '법원읍', label: '법원읍' },
+    { value: '연다산동', label: '연다산동' },
     { value: '조리읍', label: '조리읍' },
-    { value: '연다르동', label: '연다르동' },
+    { value: '파주읍', label: '파주읍' },
   ],
   평택시: [
-    { value: '평택동', label: '평택동' },
     { value: '서정동', label: '서정동' },
     { value: '중앙동', label: '중앙동' },
-    { value: '서정동', label: '서정동' },
-    { value: '서정동', label: '서정동' },
+    { value: '평택동', label: '평택동' },
   ],
   화성시: [
     { value: '봉담읍', label: '봉담읍' },
@@ -139,38 +144,31 @@ export const DISTRICT_OPTIONS_BY_CITY = {
   ],
   시흥시: [
     { value: '대야동', label: '대야동' },
-    { value: '산들동', label: '산들동' },
     { value: '은행동', label: '은행동' },
+    { value: '장곡동', label: '장곡동' },
     { value: '정왕동', label: '정왕동' },
     { value: '정자동', label: '정자동' },
-    { value: '정발산동', label: '정발산동' },
-    { value: '화정동', label: '화정동' },
-    { value: '평동', label: '평동' },
-    { value: '포동', label: '포동' },
-    { value: '장곡동', label: '장곡동' },
-  ],
-  남양주시: [
-    { value: '별내읍', label: '별내읍' },
-    { value: '단월동', label: '단월동' },
-    { value: '별내동', label: '별내동' },
-    { value: '오남읍', label: '오남읍' },
-    { value: '퇴계원읍', label: '퇴계원읍' },
-    { value: '일산동구', label: '일산동구' },
   ],
 
+  남양주시: [
+    { value: '별내동', label: '별내동' },
+    { value: '퇴계원읍', label: '퇴계원읍' },
+    { value: '평내동', label: '평내동' },
+    { value: '호평동', label: '호평동' },
+  ],
   // 인천광역시
   계양구: [
     { value: '계산동', label: '계산동' },
     { value: '작전동', label: '작전동' },
   ],
   연수구: [
-    { value: '송도동', label: '송도동' },
     { value: '선학동', label: '선학동' },
+    { value: '송도동', label: '송도동' },
   ],
   중구: [
     { value: '신포동', label: '신포동' },
-    { value: '중앙동', label: '중앙동' },
     { value: '신흥동', label: '신흥동' },
+    { value: '중앙동', label: '중앙동' },
   ],
 };
 
@@ -181,6 +179,8 @@ const DEFAULT_DISTRICT_OPTIONS = [{ value: '기타', label: '기타' }];
 export const CITY_OPTIONS = [
   { value: '', label: '전체' },
   { value: '강남구', label: '강남구' },
+  { value: '강동구', label: '강동구' },
+  { value: '광진구', label: '광진구' },
   { value: '마포구', label: '마포구' },
   { value: '서대문구', label: '서대문구' },
   { value: '서초구', label: '서초구' },
