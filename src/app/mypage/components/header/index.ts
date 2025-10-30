9157060f7b2b8b7484a677642060883111f8689b
keeprok <140785214+keeprok@users.noreply.github.com>
2025-10-30 13:38:11 +0900

    remove: 중복파일삭제

--- src/app/mypage/components/header/index.ts
+++
@@ -1,4 +0,0 @@
-export { MypageHeader } from './MypageHeader';
-export { MainTabSection } from './MainTabSection';
-export { SubTabSection } from './SubTabSection';
-export { ReviewToggleButton } from './ReviewToggleButton';
