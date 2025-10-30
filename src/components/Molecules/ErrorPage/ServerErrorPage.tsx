bcee723af6c4685bf19b8ed4e4bb875714d38f85
keeprok <140785214+keeprok@users.noreply.github.com>
2025-10-31 02:40:38 +0900

    design: serverErrorPage 추가

---
+++ src/components/Molecules/ErrorPage/ServerErrorPage.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@/components';
+import Image from 'next/image';
+
+export const ServerErrorPage = () => {
+  const onButtonClick = () => (window.location.href = '/');
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 py-20">
+      <Image
+        src="/images/error_500_image.svg"
+        alt="서버 에러 이미지"
+        width={200}
+        height={200}
+        className="mx-auto"
+      />
+
+      <p className="leading-tight font-semibold">
+        서버가 잠시 멈췄습니다.
+        <br />
+        기술팀이 이미 달려가고 있어요!
+      </p>
+      <Button label="홈으로" onClick={onButtonClick} variant="outline" />
+    </div>
+  );
+};
