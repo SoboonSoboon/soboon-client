abcd113c3d7b4430c4622ecd406b4323a455698b
jihee <sweet9811@gmail.com>
2025-10-30 13:21:23 +0900

    style: header가 addinfo에서는 나오지않도록 수정

---
+++ src/app/Headercontainer.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+export default function HeaderContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const segments = pathname.split('/');
+  console.log(segments);
+
+  if (segments[2] === 'addinfo') {
+    return null;
+  }
+
+  return <>{children}</>;
+}
