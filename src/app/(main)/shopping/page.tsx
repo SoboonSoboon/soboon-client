a0fa8ba1f1b08601d9421cf6da951c9c7d23702c
misung.dev <misung.dev@gmail.com>
2025-10-31 15:41:30 +0900

    style: 장보기 이미지 반응형 구현

--- src/app/(main)/shopping/page.tsx
+++ src/app/(main)/shopping/page.tsx
@@ -103,7 +103,7 @@ export default async function ShoppingPage({
         title="장보기"
         src="/images/banner_shopping.png"
         alt="장보기 배너"
-        imageTopClassName="top-[-8px] sm:top-[-16px] md:top-[-24px] lg:top-[-32px]"
+        imageTopClassName="top-[-12px] sm:top-[-14px] md:top-[-16px] lg:top-[-18px] "
       />
       <section className="flex flex-col gap-4 md:gap-6">
         <ShoppingTagsSection />
