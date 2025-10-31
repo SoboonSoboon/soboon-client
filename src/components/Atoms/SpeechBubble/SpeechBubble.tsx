c8c21b816ebdd0fceac45f89f79556d66e76013b
jihee <sweet9811@gmail.com>
2025-10-31 18:05:55 +0900

    style: 시작하기 말풍선 작성

---
+++ src/components/Atoms/SpeechBubble/SpeechBubble.tsx
@@ -0,0 +1,37 @@
+// SpeechBubble.tsx
+import { ReactNode } from 'react';
+
+interface SpeechBubbleProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export default function SpeechBubble({
+  children,
+  className = '',
+}: SpeechBubbleProps) {
+  return (
+    <div className={`relative ${className}`}>
+      {/* 말풍선 본체 */}
+      <div className="bg-gray-80 text-normal relative rounded-lg px-4 py-2 text-base text-white">
+        {children}
+
+        {/* 말풍선 꼬리 (위 중앙) */}
+        <div className="absolute top-1 left-47 -translate-y-full xl:left-1/2 xl:-translate-x-1/2">
+          <svg
+            width="13"
+            height="12"
+            viewBox="0 0 13 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4.60096 0.999997C5.37076 -0.333336 7.29526 -0.333333 8.06506 1L12.3952 8.5C13.165 9.83334 12.2027 11.5 10.6631 11.5L2.00288 11.5C0.463277 11.5 -0.49897 9.83333 0.27083 8.5L4.60096 0.999997Z"
+              fill="#4E4E4E"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
