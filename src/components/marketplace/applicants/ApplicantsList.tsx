d2d416467365d61debdabd31437cdd8656055346
kim dong hyeon <kdh990315@naver.com>
2025-11-01 02:35:18 +0900

    fix: 신청자 관련 오류 해결

--- src/components/marketplace/applicants/ApplicantsList.tsx
+++ src/components/marketplace/applicants/ApplicantsList.tsx
@@ -95,7 +95,7 @@ export const ApplicantsList = ({
 
   return (
     <>
-      {isAuthor && (
+      {isAuthor && !isCompletedOrClosed && (
         <div className="border-gray-10 mb-5 w-full rounded-xl border bg-white">
           {participants.length === 0 && (
             <div className="text-text-sub2 flex min-h-[143px] items-center justify-center">
@@ -123,62 +123,80 @@ export const ApplicantsList = ({
                     <div className="text-primary text-sm font-semibold">
                       참여 확정
                     </div>
-                    {!isCompletedOrClosed && (
-                      <Button
-                        label="강퇴"
-                        size="small"
-                        className="!border-text-sub2 !text-text-sub2"
-                        variant="outline"
-                        onClick={() =>
-                          handleKickApplicants(participant.participantId)
-                        }
-                      />
-                    )}
+                    <Button
+                      label="강퇴"
+                      size="small"
+                      className="!border-text-sub2 !text-text-sub2"
+                      variant="outline"
+                      onClick={() =>
+                        handleKickApplicants(participant.participantId)
+                      }
+                    />
                   </div>
                 )}
 
-                {!isCompletedOrClosed && (
-                  <>
-                    {/* 참여자 신청 대기 상태 */}
-                    {participant.status === 'APPLIED' && (
-                      <div className="flex gap-2">
-                        <Button
-                          label="수락"
-                          size="small"
-                          variant="outline"
-                          onClick={() =>
-                            handleApproveApplicants(participant.participantId)
-                          }
-                        />
-                        <Button
-                          label="거절"
-                          className="!border-text-sub2 !text-text-sub2"
-                          variant="outline"
-                          size="small"
-                          onClick={() =>
-                            handleRejectApplicants(participant.participantId)
-                          }
-                        />
-                      </div>
-                    )}
+                {/* 참여자 신청 대기 상태 */}
+                {participant.status === 'APPLIED' && (
+                  <div className="flex gap-2">
+                    <Button
+                      label="수락"
+                      size="small"
+                      variant="outline"
+                      onClick={() =>
+                        handleApproveApplicants(participant.participantId)
+                      }
+                    />
+                    <Button
+                      label="거절"
+                      className="!border-text-sub2 !text-text-sub2"
+                      variant="outline"
+                      size="small"
+                      onClick={() =>
+                        handleRejectApplicants(participant.participantId)
+                      }
+                    />
+                  </div>
+                )}
 
-                    {/* 참여자 강퇴 상태 */}
-                    {participant.status === 'KICKED' && (
-                      <div className="text-text-sub2 text-sm font-semibold">
-                        강퇴된 참여자
-                      </div>
-                    )}
+                {/* 참여자 강퇴 상태 */}
+                {participant.status === 'KICKED' && (
+                  <div className="text-text-sub2 text-sm font-semibold">
+                    강퇴된 참여자
+                  </div>
+                )}
 
-                    {/* 참여자 거절 상태 */}
-                    {participant.status === 'REJECTED' && (
-                      <div className="text-text-sub2 text-sm font-semibold">
-                        거절된 참여자
-                      </div>
-                    )}
-                  </>
+                {/* 참여자 거절 상태 */}
+                {participant.status === 'REJECTED' && (
+                  <div className="text-text-sub2 text-sm font-semibold">
+                    거절된 참여자
+                  </div>
                 )}
               </div>
             ))}
+        </div>
+      )}
+
+      {isAuthor && isCompletedOrClosed && (
+        <div className="border-gray-10 mb-5 w-full rounded-xl border bg-white">
+          {filteredParticipants?.map((participant) => (
+            <div
+              key={participant.participantId}
+              className="flex items-center justify-between px-6 py-3"
+            >
+              <div className="flex items-center gap-2 py-2">
+                <ProfileImg
+                  profileImageUrl={participant.profileImageUrl}
+                  size={32}
+                />
+                <p>{participant.userNickname}</p>
+              </div>
+              <div className="flex items-center gap-2.5">
+                <div className="text-primary text-sm font-semibold">
+                  참여 확정
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       )}
 
