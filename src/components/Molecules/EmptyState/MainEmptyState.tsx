2b6bfa51bc57426d72d32d781eb70f0d54e0c713
misung.dev <misung.dev@gmail.com>
2025-11-01 23:23:54 +0900

    design: 조건에 맞는 모임이 없을 경우 나타나는 메시지와 이미지 변경

--- src/components/Molecules/EmptyState/MainEmptyState.tsx
+++ src/components/Molecules/EmptyState/MainEmptyState.tsx
@@ -4,7 +4,6 @@ import { Button } from '@/components';
 import Image from 'next/image';
 
 export interface MainEmptyStateProps {
-  title: string;
   description: string;
   primaryButton: {
     text: string;
@@ -21,7 +20,6 @@ export interface MainEmptyStateProps {
 
 export const MainEmptyState = (props: MainEmptyStateProps) => {
   const {
-    title,
     description,
     primaryButton,
     secondaryButton,
@@ -32,21 +30,19 @@ export const MainEmptyState = (props: MainEmptyStateProps) => {
     <div className={`flex flex-col items-center justify-center ${padding}`}>
       <div className="flex flex-col items-center justify-center text-center">
         <Image
-          src="/images/empty_image.png"
+          src="/images/empty_image.svg"
           alt="빈 데이터 이미지"
           width={200}
           height={200}
           className="mx-auto"
         />
-        <div className="mb-8 flex flex-col items-center justify-center gap-2">
-          <p>{title}</p>
-          <p>{description}</p>
-        </div>
+        <p className="mb-8">{description}</p>
       </div>
       <div className="flex flex-col gap-3">
         <Button
           onClick={() => (window.location.href = primaryButton.href)}
           variant={primaryButton.variant || 'filled'}
+          className="w-[256px]"
         >
           {primaryButton.text}
         </Button>
