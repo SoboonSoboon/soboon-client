7b5d668d8a33a4df0213decf126f1b05eee4d83b
kim dong hyeon <kdh990315@naver.com>
2025-11-03 04:18:46 +0900

    fix: next/font 사용

---
+++ src/app/fonts.ts
@@ -0,0 +1,18 @@
+import localFont from 'next/font/local';
+
+export const memomentKkukkkuk = localFont({
+  src: [
+    {
+      path: '../../public/fonts/memomentKkukkkuk.woff2',
+    },
+  ],
+  variable: '--font-memoment-kkukkkuk',
+});
+export const pretendard = localFont({
+  src: [
+    {
+      path: '../../public/fonts/PretendardVariable.woff2',
+    },
+  ],
+  variable: '--font-pretendard',
+});
