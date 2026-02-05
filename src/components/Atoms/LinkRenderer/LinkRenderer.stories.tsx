5d21911085ee53a261910c731ef01d2cf4cbd2f3
misung-dev <misung.dev@gmail.com>
2026-02-05 17:10:40 +0900

    remove: netlify 관련 설정 제거 (#442)
    * remove: netlify 관련 설정 제거
    
    * fix: 레포에 vercel.json을 추가해서 Next.js로 강제 + 출력 디렉토리 .next 지정

--- src/components/Atoms/LinkRenderer/LinkRenderer.stories.tsx
+++ src/components/Atoms/LinkRenderer/LinkRenderer.stories.tsx
@@ -21,13 +21,13 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const singleShortLinkText =
-  '짧은 링크가 어떻게 본문에 들어가는지 확인해보세요: https://soboon.netlify.app/';
+  '짧은 링크가 어떻게 본문에 들어가는지 확인해보세요: https://soboon-client-seven.vercel.app';
 
 const singleLongLinkText =
   '긴 링크가 어떻게 본문에 들어가는지 확인해보세요: https://zemmaworld.com/product/mmmm-2%EC%B0%A8%EC%98%A4%ED%94%88%ED%94%BC%EC%B9%B4-%EC%86%8C%ED%94%84%ED%8A%B8%EC%8A%A4%EC%9B%A8%EC%9D%B4%EB%93%9C-%EB%B8%94%EB%A3%A8%EC%A2%85size12/32153/category/918/display/1/cafe_mkt=ue_naver_hot_1?NaPm=ct%3Dmh69lgtk%7Cci%3D7749a6056bdce2bee4b2292c74f985bbc985b6cc%7Ctr%3Dsbsh%7Csn%3D180496%7Cic%3D%7Chk%3D0b879c7a6a70965685973bc7c78a7ffe3945e933';
 
 const multipleLinksText =
-  '짧은 링크가 어떻게 본문에 들어가는지 확인해보세요: https://soboon.netlify.app/ 긴 링크가 어떻게 본문에 들어가는지 확인해보세요: https://zemmaworld.com/product/mmmm-2%EC%B0%A8%EC%98%A4%ED%94%88%ED%94%BC%EC%B9%B4-%EC%86%8C%ED%94%84%ED%8A%B8%EC%8A%A4%EC%9B%A8%EC%9D%B4%EB%93%9C-%EB%B8%94%EB%A3%A8%EC%A2%85size12/32153/category/918/display/1/cafe_mkt=ue_naver_hot_1?NaPm=ct%3Dmh69lgtk%7Cci%3D7749a6056bdce2bee4b2292c74f985bbc985b6cc%7Ctr%3Dsbsh%7Csn%3D180496%7Cic%3D%7Chk%3D0b879c7a6a70965685973bc7c78a7ffe3945e933 텍스트 중간에 있는 링크도 잘 표시되는지 확인해보세요.';
+  '짧은 링크가 어떻게 본문에 들어가는지 확인해보세요: https://soboon-client-seven.vercel.app 긴 링크가 어떻게 본문에 들어가는지 확인해보세요: https://zemmaworld.com/product/mmmm-2%EC%B0%A8%EC%98%A4%ED%94%88%ED%94%BC%EC%B9%B4-%EC%86%8C%ED%94%84%ED%8A%B8%EC%8A%A4%EC%9B%A8%EC%9D%B4%EB%93%9C-%EB%B8%94%EB%A3%A8%EC%A2%85size12/32153/category/918/display/1/cafe_mkt=ue_naver_hot_1?NaPm=ct%3Dmh69lgtk%7Cci%3D7749a6056bdce2bee4b2292c74f985bbc985b6cc%7Ctr%3Dsbsh%7Csn%3D180496%7Cic%3D%7Chk%3D0b879c7a6a70965685973bc7c78a7ffe3945e933 텍스트 중간에 있는 링크도 잘 표시되는지 확인해보세요.';
 
 export const Default: Story = {
   args: {
