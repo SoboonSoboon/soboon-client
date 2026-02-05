51ff15eea2387bd2f4b6e417d937ae9e31fcb61c
misung.dev <misung.dev@gmail.com>
2026-02-05 16:21:59 +0900

    chore: 토큰 만료 등의 이슈로 스토리북 재설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,14 @@
-import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/nextjs-vite",
-    "options": {}
-  },
-  "staticDirs": [
-    "../public"
-  ]
+  framework: '@storybook/nextjs-vite',
+  staticDirs: ['../public'],
 };
 export default config;
